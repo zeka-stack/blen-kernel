688d1973e6e9b96ad6978d44593d1fc98f905279
dong4j <dong4j@gmail.com>
2025-10-07 14:02:52 +0800

    refactor(db): 统一时间字段类型为datetime
    - 将所有表中的create_time和update_time字段类型从timestamp改为datetime

--- blen-kernel-devtools/src/test/db/ddl.sql
+++ blen-kernel-devtools/src/test/db/ddl.sql
@@ -8,10 +8,10 @@ create table rule
     `match_mode`    varchar(20)  null comment '域名匹配模式',
     `name`          varchar(100) null comment '匹配的域名',
     `priority`      int          null comment '匹配优先级',
-    `enabled` bit default b'1' null comment '公共枚举:EnabledEnum:可用状态',
+    `enabled`     bit               default b'1' null comment '公共枚举:EnabledEnum:可用状态',
     `dispatch_mode` varchar(20)  null comment '分发模式, 如iphash、round-robin、random',
-    `create_time`   timestamp    not null default current_timestamp comment '创建时间 (公共字段)',
-    `update_time`   timestamp    not null default current_timestamp on update current_timestamp comment '最后更新时间 (公共字段)',
+    `create_time` datetime not null default current_timestamp comment '创建时间 (公共字段)',
+    `update_time` datetime not null default current_timestamp on update current_timestamp comment '最后更新时间 (公共字段)',
     `deleted`       bigint       not null default 0 comment '通用枚举:删除状态:0: 未删除，删除就是当前数据的主键id用于代表唯一性'
 ) engine = INNODB
   auto_increment = 0
@@ -32,8 +32,8 @@ create table user
     `secret_key`   varchar(50)  null comment 'open api secret key',
     `email`        varchar(128) null comment '用户邮箱',
     `state`        tinyint(2) unsigned   default 0 not null comment '自定义枚举:Integer:用户状态:AAA(0, "未审核"),:BBB(1, "审核中"),:CCC(2, "审核未通过"),:DDD(3,"已锁定"),:EEE(4,"正常");',
-    `create_time`  timestamp    not null default current_timestamp comment '创建时间 (公共字段)',
-    `update_time`  timestamp    not null default current_timestamp on update current_timestamp comment '最后更新时间 (公共字段)',
+    `create_time` datetime not null default current_timestamp comment '创建时间 (公共字段)',
+    `update_time` datetime not null default current_timestamp on update current_timestamp comment '最后更新时间 (公共字段)',
     `deleted`      bigint       not null default 0 comment '通用枚举:删除状态:0: 未删除，删除就是当前数据的主键id用于代表唯一性'
 ) engine = INNODB
   auto_increment = 0
@@ -56,10 +56,10 @@ create table example
     `name`        varchar(100) null comment 'Q@姓名',
     `gender`      tinyint(2) unsigned   default 2 not null comment 'Q@自定义枚举:Integer:性别:WOMAN(0, "女性"),:MAN(1, "男性"),:UNKNOWN(2, "未知");',
     `hobby`       tinyint(2) unsigned   default 2 not null comment '自定义枚举:Integer:爱好:WOMAN(0, "女性"),:MAN(1, "男性"),:UNKNOWN(2, "未知");',
-    `birthday`    timestamp    not null comment '生日',
-    `enabled` bit default b'1' null comment '公共枚举:EnabledEnum:可用状态',
-    `create_time` timestamp    not null default current_timestamp comment '创建时间 (公共字段)',
-    `update_time` timestamp    not null default current_timestamp on update current_timestamp comment '最后更新时间 (公共字段)',
+    `birthday`    datetime not null comment '生日',
+    `enabled`     bit               default b'1' null comment '公共枚举:EnabledEnum:可用状态',
+    `create_time` datetime not null default current_timestamp comment '创建时间 (公共字段)',
+    `update_time` datetime not null default current_timestamp on update current_timestamp comment '最后更新时间 (公共字段)',
     `deleted`     bigint       not null default 0 comment '通用枚举:删除状态:0: 未删除，删除就是当前数据的主键id用于代表唯一性'
 ) engine = INNODB
   auto_increment = 0
